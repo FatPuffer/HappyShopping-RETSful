26849d09a4de33838000c6eef5c943c9dcc0d0d2
fatpuffer <fatpuffer@163.com>
2019-04-23 21:27:11 +0800

    修改serializer

--- HappyShopping/apps/goods/serializers.py
+++ HappyShopping/apps/goods/serializers.py
@@ -3,18 +3,18 @@ from rest_framework import serializers
 from goods.models import Goods, GoodsCategory
 
 
- #class GoodsSerializer(serializers.Serializer):
- #    name = serializers.CharField(required=True, max_length=100)
- #    click_num = serializers.IntegerField(default=0)
- #    goods_front_image = serializers.ImageField()
- #
- #    # drf 默认会将所有字段值放在 validated_data 参数中
- #    def create(self, validated_data):
- #        """
- #        创建对象
- #        """
- #        return Goods.objects.create(**validated)
- #
+# class GoodsSerializer(serializers.Serializer):
+#    name = serializers.CharField(required=True, max_length=100)
+#    click_num = serializers.IntegerField(default=0)
+#    goods_front_image = serializers.ImageField()
+#
+#    # drf 默认会将所有字段值放在 validated_data 参数中
+#    def create(self, validated_data):
+#        """
+#        创建对象
+#        """
+#        return Goods.objects.create(**validated)
+#
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -26,6 +26,7 @@ class CategorySerializer(serializers.ModelSerializer):
 class GoodsSerializer(serializers.ModelSerializer):
     # Goods模型类的外键，实现序列化嵌套
     category = CategorySerializer()
+
     class Meta:
         model = Goods
         fields = "__all__"
